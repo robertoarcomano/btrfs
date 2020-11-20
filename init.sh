b6cae4ea44e7a659432bf82eb022649704ec0917
Roberto Arcomano <bertolinux@gmail>
2020-11-20 23:02:54 +0100

    Tests with subvolumes2

--- init.sh
+++ init.sh
@@ -7,8 +7,10 @@ FILE=file
 LOOP=$(losetup -f)
 DIR=$PWD/btrfs_dir
 LABEL=mylabel
-SUBVOLUME=subvolume
-SUBVOLUME_DIR=$PWD/subvolume_dir
+SUBVOLUME1=subvolume1
+SUBVOLUME2=subvolume2
+SUBVOLUME1_DIR=$PWD/subvolume1_dir
+SUBVOLUME2_DIR=$PWD/subvolume2_dir
 echo $LOOP
 echo $PWD/$FILE
 dd if=/dev/zero of=$FILE bs=1M count=100
@@ -22,7 +24,8 @@ sudo mkfs.btrfs $LOOP
 
 # 4. Create dirs
 mkdir -p $DIR
-mkdir -p $SUBVOLUME_DIR
+mkdir -p $SUBVOLUME1_DIR
+mkdir -p $SUBVOLUME2_DIR
 
 # 5. Mount btrfs on local dir
 sudo mount $LOOP $DIR
@@ -36,14 +39,23 @@ echo "New label: "
 sudo btrfs filesystem label $DIR
 
 # 7. Create Subvolume test
-sudo btrfs subvolume create $DIR/$SUBVOLUME
-sudo chmod -R 777 $DIR/$SUBVOLUME
-sudo echo "subvolume" > $DIR/$SUBVOLUME/subvolume.txt
+sudo btrfs subvolume create $DIR/$SUBVOLUME1
+sudo btrfs subvolume create $DIR/$SUBVOLUME2
+sudo chmod -R 777 $DIR/$SUBVOLUME1
+sudo chmod -R 777 $DIR/$SUBVOLUME2
+sudo echo "subvolume1" > $DIR/$SUBVOLUME1/subvolume1.txt
+sudo echo "subvolume2" > $DIR/$SUBVOLUME2/subvolume2.txt
 sudo umount $DIR
-sudo mount $LOOP $SUBVOLUME_DIR -o subvol=$SUBVOLUME
-find $SUBVOLUME_DIR
+
+sudo mount $LOOP $SUBVOLUME1_DIR -o subvol=$SUBVOLUME1
+find $SUBVOLUME1_DIR
+sudo mount $LOOP $SUBVOLUME2_DIR -o subvol=$SUBVOLUME2
+find $SUBVOLUME2_DIR
 sudo df -h
 
 # 99. Unmount and remove connection file / loop device
-sudo umount $SUBVOLUME_DIR
+sleep 1
+sudo umount $SUBVOLUME2_DIR
+sleep 1
+sudo umount $SUBVOLUME1_DIR
 sudo losetup -d $LOOP
